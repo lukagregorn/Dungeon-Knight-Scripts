97e1c6c0ec9898cd701ac775d0de64805f10b432
lukagregorn <gregorn.luka@gmail.com>
2020-10-24 20:57:59 +0200

    Initial commit

---
+++ PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // reset change
+        change = Vector3.zero;
+
+        // get new change
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+
+        // move character
+        UpdateAnimationAndMove();
+    }
+
+
+    void UpdateAnimationAndMove() {
+        if (change != Vector3.zero) {
+            MoveCharacter();
+
+            // animator blend tree changes
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        } else {
+            animator.SetBool("moving", false);
+        }
+    }
+
+
+    void MoveCharacter()
+    {
+        myRigidbody.MovePosition(
+            transform.position + change * speed * Time.deltaTime
+        );
+    }
+}
