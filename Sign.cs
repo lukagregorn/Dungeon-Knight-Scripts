4571d61457cc10990309987cada82f6546329d03
lukagregorn <gregorn.luka@gmail.com>
2020-10-25 18:13:46 +0100

    Added zone movers, sign, and camera movement

---
+++ Sign.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : MonoBehaviour
+{
+
+    public GameObject questionMark;
+    public Vector3 questionMarkOffset;
+
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool dialogActive;
+
+    private bool playerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange) {
+
+            if (dialogBox.activeInHierarchy) {
+                
+                dialogBox.SetActive(false);
+                ShowQuestionMark();
+
+            } else {
+
+                HideQuestionMark();
+
+                dialogText.text = dialog;
+                dialogBox.SetActive(true);
+            }
+
+        }
+    }
+
+    
+    private void ShowQuestionMark() {
+        questionMark.transform.position = this.transform.position + questionMarkOffset;
+        questionMark.SetActive(true);
+    }
+
+
+    private void HideQuestionMark() {
+        questionMark.SetActive(false);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+
+        if (other.CompareTag("Player")) {
+
+            playerInRange = true;
+            ShowQuestionMark();
+
+        }
+
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other) {
+
+        if (other.CompareTag("Player")) {
+
+            playerInRange = false;
+            
+            dialogBox.SetActive(false);
+            HideQuestionMark();
+
+        }
+
+    }
+}
---
+++ ZoneMove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZoneMove : MonoBehaviour
+{
+
+    public Vector2 cameraChangeMax;
+    public Vector2 cameraChangeMin;
+    public Vector3 playerChange;
+    public bool needText;
+    public string zoneName;
+    public GameObject text;
+    public Text zoneText;
+
+    private CameraMovement cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            cam.maxPosition = cameraChangeMax;
+            cam.minPosition = cameraChangeMin;
+            other.transform.position += playerChange;
+
+            if (needText) {
+                StartCoroutine(zoneNameCoroutine());
+            }
+        }
+    }
+
+    // coroutine for showing zone name
+    private IEnumerator zoneNameCoroutine() {
+        // change text to new zone text
+        zoneText.text = zoneName;
+
+        // set active and inactive
+        text.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        text.SetActive(false);
+    }
+
+}
