b2edeb984db5b63bafa60fb02078410c76a3f07b
lukagregorn <gregorn.luka@gmail.com>
2020-12-08 23:48:31 +0100

    added pathfinding to enemies

--- Humanoids/Enemies/Enemy.cs
+++ Humanoids/Enemies/Enemy.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Pathfinding;
 
 
 public class Enemy : Humanoid
@@ -14,29 +15,65 @@ public class Enemy : Humanoid
     public UnityEvent DiedEvent;
     protected int health;
     
+    protected Seeker seeker;
+    protected Path path;
+    protected int currentWaypoint = 0;
+    protected bool reachedEndOfPath = false;
+    private float nextWaypointDistance = 1.5f;
 
     private void Start() {
         health = maxHealth.value;
         GameObject p = GameObject.FindWithTag("Player");
-        if (p) {
-            target = p.transform;
-        } else {
+        if (!p) {
             Debug.LogWarning("No player transform");
+            return;
         }
+
+        // ai
+        target = p.transform;
+        seeker = GetComponent<Seeker>();
+
+        // generate path
+        InvokeRepeating("UpdatePath", 0f, .5f);
     }
 
 
     // MOVEMENT
-    protected Vector3 MoveTowardsTarget() {
+    protected void UpdatePath() {
+        if (seeker.IsDone())
+            seeker.StartPath(myRigidbody.position, target.position, OnPathComplete);
+    }
+
+    protected bool ReachedPathEnd() {
+        reachedEndOfPath = currentWaypoint >= path.vectorPath.Count;
+        return reachedEndOfPath;
+    }
+
+    protected void OnPathComplete(Path p) {
+        if (!p.error) {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    protected Vector2 MoveTowardsTarget() {
         HumanoidState state = GetState();
         if (state == HumanoidState.idle || state == HumanoidState.walk) {
-            Vector3 tmp = Vector3.MoveTowards(transform.position, target.position, speed.value * Time.deltaTime);
-            myRigidbody.MovePosition(tmp);
+            Vector2 direction = ((Vector2) path.vectorPath[currentWaypoint] - myRigidbody.position).normalized;
+            Vector2 force = direction * speed.value * Time.deltaTime;
+
+            myRigidbody.AddForce(force);
+
+            float distance = Vector2.Distance(myRigidbody.position, path.vectorPath[currentWaypoint]);
+
+            if (distance < nextWaypointDistance) {
+                currentWaypoint ++;
+            }
 
             ChangeState(HumanoidState.walk);
 
             // returns direction
-            return tmp - transform.position;
+            return direction;
         }
 
         return Vector3.zero;
--- Humanoids/Enemies/Log.cs
+++ Humanoids/Enemies/Log.cs
@@ -8,43 +8,38 @@ public class Log : Enemy
     // Update is called once per frame
     private void FixedUpdate()
     {
-        if (IsInChaseRadius()) {
-            animator.SetBool("wakeUp", true);
 
-            if (!IsInAttackRadius()) {
-                Vector3 direction = MoveTowardsTarget();
-                
-                // determine which animation to play
-                if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y)) {
-                    if (direction.x > 0) {
-                        animator.SetFloat("moveX", 1f);
-                        animator.SetFloat("moveY", 0f);
-                    } else {
-                        animator.SetFloat("moveX", -1f);
-                        animator.SetFloat("moveY", 0f);
-                    }
-
-                } else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y)) {
-                    if (direction.y > 0) {
-                        animator.SetFloat("moveX", 0f);
-                        animator.SetFloat("moveY", 1f);
-                    } else {
-                        animator.SetFloat("moveX", 0f);
-                        animator.SetFloat("moveY", -1f);
-                    }
-                }
-
-
-            } else {
-                //Debug.Log("Attack");
+        if (path != null) {
+            if (!ReachedPathEnd()) {
+                Vector2 direction = MoveTowardsTarget();
+                AnimateSpriteDirection(direction);
             }
-        } else {
-            animator.SetBool("wakeUp", false);
         }
+
     }
 
 
     // MOVEMENT
+    private void AnimateSpriteDirection(Vector2 direction) {
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y)) {
+            if (direction.x > 0) {
+                animator.SetFloat("moveX", 1f);
+                animator.SetFloat("moveY", 0f);
+            } else {
+                animator.SetFloat("moveX", -1f);
+                animator.SetFloat("moveY", 0f);
+            }
+
+        } else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y)) {
+            if (direction.y > 0) {
+                animator.SetFloat("moveX", 0f);
+                animator.SetFloat("moveY", 1f);
+            } else {
+                animator.SetFloat("moveX", 0f);
+                animator.SetFloat("moveY", -1f);
+            }
+        }
+    }
 
 
     // COMBAT
